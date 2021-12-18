649264711f21d7cfbd296b3a799f57fc38ef530a
Gorniaky <hwss.s2@gmail.com>
2021-12-18 19:33:20 -0300

    Lesson #5 - desafios

--- README.md
+++ README.md
@@ -3,6 +3,6 @@
 ## JavaScript
 
 - [x] Contador - Lesson #5
-  - [ ] Tente implementar os eventos usando o método `addEventListener`.
-  - [ ] Crie condicionais que desabilitam o botão de incrementar ou decrementar quando `count` chegar a um determinado valor.
-  - [ ] Mude a cor do texto em `CURRENT_NUMBER` para vermelho quando o número for negativo.
+  - [x] Tente implementar os eventos usando o método `addEventListener`.
+  - [x] Crie condicionais que desabilitam o botão de incrementar ou decrementar quando `count` chegar a um determinado valor.
+  - [x] Mude a cor do texto em `CURRENT_NUMBER` para vermelho quando o número for negativo.
--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,27 @@
 let count = 0;
 
-const CURRENT_NUMBER = document.getElementById('currentNumber');
+const CURRENT_NUMBER = document.getElementById('currentNumber'),
+  ADD = document.getElementById('add'),
+  SUB = document.getElementById('sub');
 
 function increment() {
-	count++;
-	CURRENT_NUMBER.innerHTML = count;
+  if (count === 10) return;
+
+  count++;
+  if (count === 0)
+    CURRENT_NUMBER.style.color = 'black';
+
+  CURRENT_NUMBER.innerHTML = count;
 }
+ADD.addEventListener('click', increment);
 
 function decrement() {
-	count--;
-	CURRENT_NUMBER.innerHTML = count;
+  if (count < 0) return;
+
+  count--;
+  if (count === -1)
+    CURRENT_NUMBER.style.color = 'red';
+
+  CURRENT_NUMBER.innerHTML = count;
 }
+SUB.addEventListener('click', decrement);
