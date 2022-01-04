a792b22abb06b085a4853b8280ed164a5881bbca
Gorniaky <hwss.s2@gmail.com>
2022-01-04 16:54:54 -0300

    Lesson #12

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -7,17 +7,18 @@
  */
 
 import { NextFunction, Request, Response, Router } from "express";
+import { StatusCodes } from "http-status-codes";
 
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
   const users = [{ username: 'Renan' }];
-  res.status(200).send({ users })
+  res.status(StatusCodes.OK).send({ users })
 })
 
 usersRoute.get('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
   const { uuid } = req.params;
-  res.status(200).send({ uuid })
+  res.status(StatusCodes.OK).send({ uuid })
 })
 
 export default usersRoute;
