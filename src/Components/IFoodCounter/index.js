db394bba888bf56ee38dcd74a34ad3c8a9cdd9e0
Gorniaky <hwss.s2@gmail.com>
2022-01-04 00:18:09 -0300

    Lesson #8

--- src/Components/IFoodCounter/index.js
+++ src/Components/IFoodCounter/index.js
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './IFoodCounter.css';
 
 export default function IFoodCounter() {
   const [value, setValue] = useState(1);
   const [buttonStyle, setButtonStyle] = useState('counter-button-minus-active')
+
+  useEffect(() => console.log('buttonStyle:', buttonStyle), [buttonStyle])
 
   function down() {
     if (!value) return;
@@ -15,7 +17,7 @@ export default function IFoodCounter() {
 
   function up() {
     setValue(value + 1)
-    setButtonStyle('counter-button-plus-active')
+    setButtonStyle('counter-button-minus-active')
   }
 
   return (
@@ -23,6 +25,7 @@ export default function IFoodCounter() {
       <button className={buttonStyle} onClick={down}>-</button>
       <p>{value}</p>
       <button className="counter-button-plus-active" onClick={up}>+</button>
+      <button id='moeda'>12,00</button>
     </div>
   )
 }
