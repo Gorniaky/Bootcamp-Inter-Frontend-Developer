41a13d77ec1239b91ad9065287fc49fd28fef31f
Gorniaky <hwss.s2@gmail.com>
2022-01-04 15:53:45 -0300

    Lesson #8

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('Executei!');
+import express, { NextFunction, Request, Response } from "express";
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('http://localhost:3000')
+})
+
+app.get('/status', (req: Request, res: Response, next: NextFunction) => {
+  res.status(200).send({ foo: 'bar' });
+});
