0e146b399e67463e42b78e3a387adcb1f5611b49
Gorniaky <hwss.s2@gmail.com>
2022-01-04 17:27:30 -0300

    Lesson #14

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -12,7 +12,7 @@ import { StatusCodes } from "http-status-codes";
 const usersRoute = Router();
 
 usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
-  const users = [{ username: 'Renan' }];
+  const users = [{}];
   res.status(StatusCodes.OK).send({ users })
 })
 
@@ -24,6 +24,17 @@ usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, n
 usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
   const newUser = req.body;
   res.status(StatusCodes.CREATED).send(newUser)
+})
+
+usersRoute.put('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  const { uuid } = req.params;
+  const newUser = req.body;
+  newUser.uuid = uuid;
+  res.status(StatusCodes.OK).send(newUser);
+})
+
+usersRoute.delete('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
+  res.sendStatus(StatusCodes.OK)
 })
 
 export default usersRoute;
