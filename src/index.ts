dc55bb9f4d8685ec4d3913d5057c3bd5cf81539c
Gorniaky <hwss.s2@gmail.com>
2022-01-04 17:11:52 -0300

    Lesson #13

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { NextFunction, Request, Response } from "express";
+import express from "express";
 import usersRoute from "./routes/users";
 
 const app = express();
@@ -6,5 +6,8 @@ const app = express();
 app.listen(3000, () => {
   console.log('http://localhost:3000')
 })
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.use(usersRoute)
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -16,9 +16,14 @@ usersRoute.get('/users', (req: Request, res: Response, next: NextFunction) => {
   res.status(StatusCodes.OK).send({ users })
 })
 
-usersRoute.get('/users/:uuid', (req: Request<{uuid: string}>, res: Response, next: NextFunction) => {
+usersRoute.get('/users/:uuid', (req: Request<{ uuid: string }>, res: Response, next: NextFunction) => {
   const { uuid } = req.params;
   res.status(StatusCodes.OK).send({ uuid })
+})
+
+usersRoute.post('/users', (req: Request, res: Response, next: NextFunction) => {
+  const newUser = req.body;
+  res.status(StatusCodes.CREATED).send(newUser)
 })
 
 export default usersRoute;
