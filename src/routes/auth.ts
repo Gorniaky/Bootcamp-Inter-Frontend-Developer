5d5347c6c9ed1c573a5d46a4d1800eee671e25cd
Gorniaky <hwss.s2@gmail.com>
2022-01-04 21:10:42 -0300

    Lesson #3

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -8,12 +8,19 @@ authRoute.post('/token', (req: Request, res: Response, next: NextFunction) => {
     const { authorization } = req.headers;
 
     if (!authorization)
-      throw new ForbiddenError('Credenciais não informadas.');
+      throw new ForbiddenError('Credenciais não preenchidas.');
 
     const [auth_type, token] = authorization.split(' ');
 
     if (auth_type !== 'Basic' || !token)
-      throw new ForbiddenError('Tipo de autenticação inválido.')
+      throw new ForbiddenError('Tipo de autenticação inválido.');
+
+    const tokenContent = Buffer.from(token, 'base64').toString('utf8');
+
+    const [username, password] = tokenContent.split(':');
+
+    if (!username || !password)
+      throw new ForbiddenError('Credenciais não preenchidas.')
 
   } catch (error) {
     next(error);
